1536dfe3cf822d3c6e735b82543392e5927fbc4a
dirgalibro <dirga.libro@gmail.com>
2024-11-01 18:51:19 +0700

    Create procedure.js

---
+++ procedure.js
@@ -0,0 +1,13 @@
+Scan jaringan baru dengan Scan-analyze hingga level 5
+Koneksi ke server
+ls aja dulu siapa tau ada kontrak
+analyze (Periksa potensi uang dan tingkat keamanan minimum dari server. Ini berguna untuk menentukan apakah server layak untuk di-hack.)
+atau run ServerProfiler.exe (server name)
+Jika server memerlukan beberapa port terbuka untuk mendapatkan akses root, kamu perlu membuka port terlebih dahulu dengan tools seperti:
+BruteSSH.exe (server name)
+FTPCrack.exe (server name)
+install backdoor (perlu akses admin)
+Setelah port yang dibutuhkan terbuka, jalankan:
+NUKE.exe (server name)
+Hitung RAM yang tersedia di server untuk menentukan berapa banyak threads yang bisa digunakan untuk menjalankan script:
+grind.js
